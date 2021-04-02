66745e6c769b4644086e6e7230a1775f42437210
jonaguyo <jonaguyo@gmail.com>
2021-04-02 20:00:01 +0200

    Ip tracker end

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,8 @@ let ipifyKey = 'at_r8aldhC3xuPElC316XF8TjjBV6l2O';
 let leafletKey = 'pk.eyJ1IjoiYXJhbWluZXQiLCJhIjoiY2trODlteDlrMDV0bjJ2cTh1bjRsZm13eSJ9.6WhlUeHsibF_pfqLGxmAAA';
 let ipifyUrl = 'https://geo.ipify.org/api/v1?apiKey=' + ipifyKey + '&ipAddress=';
 
-let map = L.map('map').setView([48.85875893258228, 2.348786807381395], 11);
+let map = L.map('map', {zoomControl: false}).setView([48.85875893258228, 2.348786807381395], 11);
+let marker;
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -13,7 +14,16 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: leafletKey
 }).addTo(map);
 
+//searchIp();
 document.getElementById("search-button").addEventListener('click', searchIp);
+document.getElementById("search-field").addEventListener('keypress', handleEnter);
+
+function handleEnter(event) {
+    if(event.keyCode == 13) {
+        event.preventDefault();
+        searchIp();
+    }
+}
 
 function searchIp() {
     let ip = document.getElementById("search-field").value;
@@ -23,8 +33,23 @@ function searchIp() {
     request.onload = (e) => {
         let response = JSON.parse(request.responseText);
         document.getElementById('ip').innerText = response['ip'];
+        document.getElementById('location').innerText = 
+            response['location'].city 
+            + '\n' 
+            + response['location'].postalCode
+            + ', '
+            + response['location'].country;
+        document.getElementById('timezone').innerText = 'UTC' + response['location'].timezone;
+        document.getElementById('isp').innerText = response['isp'];
+
         let lat = response['location'].lat;
         let lng = response['location'].lng;
-        map.flyTo(new L.LatLng(lat, lng), 18);
+        if(marker == null) {
+            marker = L.marker([lat, lng]);
+            marker.addTo(map);
+        } else {
+            marker.setLatLng([lat, lng]);
+        }
+        map.flyTo(new L.LatLng(lat, lng), 15, {duration: 2});
     }
 }
